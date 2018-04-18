35f20a41f0bd3c36a32c6981bbc46e4808761b52
Zack Hartmann <zackhartmann@gmail.com>
2018-04-17 20:29:41 -0400

    Implemented New Displays

--- react-coin/src/components/list/List.js
+++ react-coin/src/components/list/List.js
@@ -36,17 +36,38 @@ class List extends React.Component {
 	}
 
 	render() {
-		console.log(this.state);
-
 		if (this.state.loading) {
 			return <div>Loading...</div>
 		}
 
 		return (
-			<div>
-				{this.state.currencies.map(currency => (
-					<div key={currency.id}>{currency.id}</div>
-				))}
+			<div className="Table-container">
+				<table className="Table">
+					<thead className="Table-head">
+						<tr>
+							<th>Cryptocurrency</th>
+							<th>Price</th>
+							<th>Market Cap</th>
+							<th>24H Change</th>
+						</tr>
+					</thead>
+					<tbody className="Table-body">
+					{this.state.currencies.map(currency => (
+						<tr key={currency.id}>
+							<td>
+								<span className="Table-rank">{currency.rank}</span>
+								{currency.name}
+							</td>
+							<td>
+								<span className="Table-dollar">$ {currency.price}</span>
+							</td>
+							<td>
+								<span className="Table-dollar">$ {currency.marketCap}</span>
+							</td>
+						</tr>
+					))}
+					</tbody>
+				</table>
 			</div>
 		)
 	}
