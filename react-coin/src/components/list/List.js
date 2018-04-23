02da468d986b2bd3c6a6f90f6b15ff715dfea050
Zack Hartmann <zackhartmann@gmail.com>
2018-04-23 12:47:15 -0400

    Extracted the 'Table' functionality to discrete file using 'prop-types'

--- react-coin/src/components/list/List.js
+++ react-coin/src/components/list/List.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { handleResponse } from '../../helpers'
 import {API_URL} from '../../config'
-import './Table.css'
 import Loading from '../common/Loading'
+import Table from './Table'
 
 class List extends React.Component {
 	constructor() {
@@ -55,39 +55,10 @@ class List extends React.Component {
 			return <div className="error">{error}</div>;
 
 		return (
-			<div className="Table-container">
-				<table className="Table">
-					<thead className="Table-head">
-						<tr>
-							<th>Cryptocurrency</th>
-							<th>Price</th>
-							<th>Market Cap</th>
-							<th>24H Change</th>
-						</tr>
-					</thead>
-					<tbody className="Table-body">
-					{currencies.map(currency => (
-						<tr key={currency.id}>
-							<td>
-								<span className="Table-rank">{currency.rank}</span>
-								{currency.name}
-							</td>
-							<td>
-								<span className="Table-dollar">$</span>
-								{currency.price}
-							</td>
-							<td>
-								<span className="Table-dollar">$</span>
-								{currency.marketCap}
-							</td>
-							<td>
-								{this.readerChangePercent(currency.percentChange24h)}
-							</td>
-						</tr>
-					))}
-					</tbody>
-				</table>
-			</div>
+			<Table
+				currencies={currencies}
+				renderChangePercent={this.readerChangePercent}
+			/>
 		)
 	}
 }
